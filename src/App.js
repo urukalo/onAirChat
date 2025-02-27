33a33bf0965826fa22a94b1a21b8bf0e47f2a31c
Milan Urukalo <urukalo@gmail.com>
2025-02-27 20:10:49 +0000

    refactor

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import OnAirChat from './components/OnAirChat';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <OnAirChat />
+    </div>
+  );
+}
+
+export default App;
